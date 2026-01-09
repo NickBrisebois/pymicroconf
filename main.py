2576fd86d2bccb7264a1d2462eabd42906e9ed42
NickBrisebois <nickbrisebois12@gmail.com>
2026-01-09 11:21:30 -0500

    chore: setup package definition

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from pymicroconf!")
-
-
-if __name__ == "__main__":
-    main()
--- pymicroconf/__init__.py
+++ pymicroconf/__init__.py
@@ -2,17 +2,20 @@
 PyMicroConf - A lightweight TOML config library with support for environment variable overrides.
 """
 
+__version__ = "0.1.0"
+
 from .config_handler import ConfigHandler
 from .exceptions import ConfigPropertyRequiredException, InvalidConfigException
-from .types import BaseConfig, ConfigField
+from .types import BaseConfig, ConfigField, ConfigType
 
-__version__ = "0.1.0"
 __author__ = "Nick Brisebois"
+__email__ = "email@nick-b.ca"
 
 __all__ = [
-    ConfigHandler,
-    BaseConfig,
-    ConfigField,
-    ConfigPropertyRequiredException,
-    InvalidConfigException,
+    "ConfigHandler",
+    "BaseConfig",
+    "ConfigField",
+    "ConfigType",
+    "ConfigPropertyRequiredException",
+    "InvalidConfigException",
 ]
